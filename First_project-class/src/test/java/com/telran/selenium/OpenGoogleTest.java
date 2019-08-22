b8b87edfbee268484f720d43ce10a1156d4f579a
nadya-hub <54092503+nadya-hub@users.noreply.github.com>
2019-08-22 19:54:49 +0300

    Added click I'm Filling Luky

--- First_project-class/src/test/java/com/telran/selenium/OpenGoogleTest.java
+++ First_project-class/src/test/java/com/telran/selenium/OpenGoogleTest.java
@@ -1,28 +1,39 @@
 package com.telran.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.openqa.selenium.By.*;
 
 public class OpenGoogleTest {
     WebDriver driver;
-    @Test
-    public void setUp() throws InterruptedException {
+    @BeforeMethod
+
+    public void setUp() {
         driver= new ChromeDriver();
         driver.get("https://www.google.com");
-        driver.findElement(name("q")).sendKeys("Java");
-        //==driver.navigate().to("https://www.google.com");
-        //driver.findElement(new ByCssSelector("input class=\"gNO89b\" ")).click();
-
-       // WebElement element = driver.findElement(By.tagName("btnK"));
-        Thread.sleep(6000);
-        driver.quit();
-
-
     }
+    @Test
+    public void searchJavaTest() throws InterruptedException {
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("Java"+ Keys.ENTER);
+        Thread.sleep(6000);
+    }
+    @Test
+    public void clickImFillingLuky() throws InterruptedException {
+        driver.findElement(By.id("fsettl")).click();
+        Thread.sleep(6000);
+    }
+@AfterMethod
+    public void tearDown(){
+    driver.quit();
+}
 }
